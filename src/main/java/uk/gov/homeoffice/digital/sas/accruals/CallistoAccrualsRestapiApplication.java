c8628583ab38a7ec463b49aacf0e37de39a95d58
joannafawl <joanna.fawl@madetech.com>
2022-10-06 16:35:43 +0100

    Create basic project

---
+++ src/main/java/uk/gov/homeoffice/digital/sas/accruals/CallistoAccrualsRestapiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.homeoffice.digital.sas.accruals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallistoAccrualsRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallistoAccrualsRestapiApplication.class, args);
+	}
+
+}
