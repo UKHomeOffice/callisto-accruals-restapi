db513218e3d98f995c3147313de4a1a48d094ad7
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2022-10-18 16:10:58 +0100

    eahw-2251-add-maven-checkstyle: remove custom config and update files to match google

--- src/main/java/uk/gov/homeoffice/digital/sas/accruals/CallistoAccrualsRestapiApplication.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/accruals/CallistoAccrualsRestapiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CallistoAccrualsRestapiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CallistoAccrualsRestapiApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(CallistoAccrualsRestapiApplication.class, args);
+  }
 
 }
