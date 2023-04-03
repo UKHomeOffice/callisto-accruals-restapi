a153a24c2b06f748c0f0ae2c22798cde3ef1ca4d
joannafawl <joanna.fawl@madetech.com>
2023-04-03 15:55:31 +0100

    Remove kafka from dev values and further refactoring

--- src/main/java/uk/gov/homeoffice/digital/sas/accruals/model/Contribution.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/accruals/model/Contribution.java
@@ -2,19 +2,13 @@ package uk.gov.homeoffice.digital.sas.accruals.model;
 
 import java.math.BigDecimal;
 import java.util.UUID;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
 @Setter
 @Getter
 public class Contribution {
 
-  // TODO: check if this annotation is needed
-  @JdbcTypeCode(SqlTypes.CHAR)
   private UUID timeEntryId;
-
   private BigDecimal value;
 }
